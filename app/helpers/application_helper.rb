2d197fcfe3ba71707d94dfc97d1bc6c54fce06cb
smcavinney <shaun.mcavinney@gmail.com>
2012-12-03 21:43:34 +0000

    added clients index table with sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def sortable(column, title = nil)
+  	title ||= column.titleize
+    css_class = column == sort_column && sort_direction == "asc" ? "icon-chevron-up" : column == sort_column && sort_direction == "desc" ? "icon-chevron-down" : nil
+  	direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to "<i class=\"#{css_class}\"></i> #{title}".html_safe, {:sort => column, :direction => direction}
+	end
 end
