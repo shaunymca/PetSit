7cad5500acd3684de9a449a88ce59f6d5812fd81
smcavinney <shaun.mcavinney@gmail.com>
2012-12-04 19:02:15 +0000

    Added non-ajax client search, ordering, and pagination. Currently pagination is set to "2", change before deployment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
   	title ||= column.titleize
     css_class = column == sort_column && sort_direction == "asc" ? "icon-chevron-down" : column == sort_column && sort_direction == "desc" ? "icon-chevron-up" : nil
   	direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to "<i class=\"#{css_class}\"></i> #{title}".html_safe, {:sort => column, :direction => direction}
+    link_to "<i class=\"#{css_class}\"></i> #{title}".html_safe, params.merge(:sort => column, :direction => direction, :page => nil)
 	end
 end
