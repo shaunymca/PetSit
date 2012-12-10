16faeb76c85687c5947058905e929481f06337f2
smcavinney <shaun.mcavinney@gmail.com>
2012-12-10 02:37:17 +0000

    Got geocoder working. Saving lat and lon for addresses added.

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,13 @@ class Client < ActiveRecord::Base
   attr_accessible :active, :address_1, :address_2, :city, :email, :first_name, :last_name, :state, :user_id, :zip
   belongs_to :user
   
+  def address
+    [address_1, address_2, city, state, zip].compact.join(', ')
+  end  
+  geocoded_by :address
+    
+  after_validation :geocode
+  
   def self.search(search)
   	if search
       where("lower(last_name) LIKE ?", "%#{search.downcase}%")
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121121192855) do
+ActiveRecord::Schema.define(:version => 20121209210739) do
 
   create_table "clients", :force => true do |t|
     t.string   "email"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(:version => 20121121192855) do
     t.datetime "updated_at", :null => false
     t.string   "first_name"
     t.string   "last_name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.boolean  "gmaps"
   end
 
   create_table "roles", :force => true do |t|
