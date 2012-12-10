145183987740ad3271e39d7a093cb1cb96c82586
smcavinney <shaun.mcavinney@gmail.com>
2012-12-10 03:59:57 +0000

    Got map embedding working with autozoom as well.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,13 +1,12 @@
 class Client < ActiveRecord::Base
   attr_accessible :active, :address_1, :address_2, :city, :email, :first_name, :last_name, :state, :user_id, :zip
   belongs_to :user
+  acts_as_gmappable :process_geocoding => false
   
-  def address
+  def fulladdress
     [address_1, address_2, city, state, zip].compact.join(', ')
   end  
-  geocoded_by :address
-    
-  after_validation :geocode
+  geocoded_by :fulladdress, :if => :fulladdressed_changed?
   
   def self.search(search)
   	if search
