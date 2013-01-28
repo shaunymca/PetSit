8df8d3a22e3578442916a4173995791541c76e45
smcavinney <shaun.mcavinney@gmail.com>
2013-01-28 19:44:51 +0000

    Default and client pricing works as expected.

--- app/models/default_price.rb
+++ app/models/default_price.rb
@@ -1,14 +1,25 @@
 class DefaultPrice < ActiveRecord::Base
-  attr_accessible :price, :user_id, :visit_type, :id, :clients_attributes
+  attr_accessible :price, :user_id, :visit_type, :id, :clients_attributes, :client_prices_attributes
   belongs_to :user
   has_many :clients, :through => :user
-  has_many :client_prices
+  has_many :client_prices, :dependent => :destroy
   accepts_nested_attributes_for :clients
+  accepts_nested_attributes_for :client_prices
+  after_create :new_client_price
+  before_update :update_clients_price
   
-  before_save do
-    client_prices.each do |client|
-      client_prices.build("price" => :price, "visit_type" => :visit_type)
+	private
+		def new_client_price
+      clients.each do |c|
+        self.client_prices.create(:price => self.price, :visit_type => self.visit_type, :client_id => c.id)
+			end
     end
-  end
   
+    def update_clients_price
+      self.client_prices.each do |p|
+          p.price = self.price
+        	p.visit_type = self.visit_type
+          p.save
+      end
+    end
 end
