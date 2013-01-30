9d5ecebf366b4342af6f0bb45f391b9695068359
smcavinney <shaun.mcavinney@gmail.com>
2013-01-30 05:19:20 +0000

    Removed need to edit user settings with password.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,11 +10,29 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update
-    role = Role.find(params[:user][:role_ids]) unless params[:user][:role_ids].nil?
-    params[:user] = params[:user].except(:role_ids)
-    super
-    resource.update_plan(role) unless role.nil?
+    # required for settings form to submit when password is left blank
+    if params[:user][:password].blank?
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+
+    @user = User.find(current_user.id)
+    if @user.update_attributes(params[:user])
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
   end
+  
+  #def update
+  #  role = Role.find(params[:user][:role_ids]) unless params[:user][:role_ids].nil?
+  #  params[:user] = params[:user].except(:role_ids)
+  #  super
+  #  resource.update_plan(role) unless role.nil?
+  #end
   
   private
   def build_resource(*args)
