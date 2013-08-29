44889b84f724f4dc02aad6c66bd0bcebfe58e8cb
smcavinney <shaun.mcavinney@gmail.com>
2013-08-29 14:33:37 +0000

    made password changes work correctly

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :authenticate_user!
   
+  def update
+    # required for settings form to submit when password is left blank
+    if params[:user][:password].blank?
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+
+    @user = User.find(current_user.id)
+    if @user.update_attributes(params[:user])
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+  
   def new
     @plan = params[:plan]
     if @plan != "admin"
