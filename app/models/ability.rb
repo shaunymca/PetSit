81484b8c4a650a4b07f35c27256ef25b4af70acc
smcavinney <shaun.mcavinney@gmail.com>
2013-12-05 17:58:53 +0000

    fixed user permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,8 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
+    elsif user.has_role? :basic || user.has_role? :standard || user.has_role? :plus || user.has_role? :advanced || user.has_role? :premier
+      can :manage, [Client, ClientPrice, DefaultPrice, Invoice, VisitBlock, Visit]
     else user.has_role? :walker
       can :read, Visit
     end
