72358ce49ace980f79936381cf749ba5f69f7156
smcavinney <shaun.mcavinney@gmail.com>
2014-02-01 22:10:50 +0000

    added default plan. 10 dollars per month

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
-    elsif user.has_role? :basic or user.has_role? :standard or user.has_role? :plus or user.has_role? :advanced or user.has_role? :premier
+    elsif user.has_role? :default or user.has_role? :basic or user.has_role? :standard or user.has_role? :plus or user.has_role? :advanced or user.has_role? :premier
       can :manage, [Client, ClientPrice, DefaultPrice, Invoice, VisitBlock, Visit]
     else user.has_role? :walker
       can :read, Visit
