a439a7d882ec5e3f43f21025518e9bdbb4638e8e
smcavinney <shaun.mcavinney@gmail.com>
2014-10-16 01:29:09 +0000

    defaulted to agenda view for mobile.

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,12 +1,12 @@
 class PrintsController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def index
     if current_user.has_role? :admin or current_user.has_role? :default or current_user.has_role? :basic or current_user.has_role? :standard or current_user.has_role? :plus or current_user.has_role? :advanced or current_user.has_role? :premier
-      @visits = current_user.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
-      @adminvisits = current_user.account.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
+      @print_visits = current_user.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
+      @admin_visits = current_user.account.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
     else
-      @visits = current_user.visits.scoped
+      @print_visits = current_user.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
     end
     respond_to do |format|
       format.html # index.html.erb
@@ -15,4 +15,3 @@ class PrintsController < ApplicationController
     end
   end
 end
-
--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -7,8 +7,11 @@ class VisitsController < ApplicationController
   def index
     if current_user.has_role? :admin or current_user.has_role? :default or current_user.has_role? :basic or current_user.has_role? :standard or current_user.has_role? :plus or current_user.has_role? :advanced or current_user.has_role? :premier
       @visits = current_user.account.visits.scoped
+      @print_visits = current_user.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
+      @admin_visits = current_user.account.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
     else
       @visits = current_user.visits.scoped
+      @print_visits = current_user.visits.where(['"visit_date" >= (:week_start) AND "visit_date" < (:weekend)', {week_start: Chronic.parse("Last week's Sunday midnight"), weekend: Chronic.parse("This week's Sunday")}]).order(:visit_date)
     end
     @visits = @visits.after(params['start']) if (params['start'])
     @visits = @visits.before(params['end']) if (params['end'])
